86257aef2e52f2295275914b5b48f0080ce644bd
2fT <2ft@s236.lan>
2023-12-07 09:53:50 +0100

    Zadanie z formularzy

--- Zadanie z formularzy/script.js
+++ Zadanie z formularzy/script.js
@@ -1,36 +1,46 @@
 function WyswietlDane()
 {
-    let imie = document.querySelector("#imie").value;
-    let nazwisko = document.querySelector("#nazwisko").value;
-    let wiek = document.querySelector("#wiek").value;
-    let email = document.querySelector("#email").value;
-    let plec = document.querySelector('input[name="plec"]:checked').id;
-    let technologia = document.querySelector("#technologia").value;
-    let jezyki = "";
-    let checkboxes = document.querySelectorAll('input[name="jezyk"]:checked');
-    for (let i = 0; i < checkboxes.length; i++) {
-        jezyki += checkboxes[i].value + ", ";
+    let rodo = document.querySelector("#rodo").checked;
+    if (rodo)
+    {
+
+        let imie = document.querySelector("#imie").value;
+        let nazwisko = document.querySelector("#nazwisko").value;
+        let wiek = document.querySelector("#wiek").value;
+        let email = document.querySelector("#email").value;
+        let plec = document.querySelector('input[name="plec"]:checked').id;
+        let technologia = document.querySelector("#technologia").value;
+        let jezyki = "";
+        let checkboxes = document.querySelectorAll('input[name="jezyk"]:checked');
+        for (let i = 0; i < checkboxes.length; i++) {
+            jezyki += checkboxes[i].value + ", ";
+        }
+        
+
+        let ocena = document.querySelector("#ocena").value;
+        let opinia = document.querySelector("#tekst").value;
+        if (opinia == "Dodaj swoją opinię o stronie...")
+        opinia = "Brak";
+        let data = document.querySelector("#data").value;
+
+
+        document.querySelector("#dane").innerHTML = `<b> Imię: </b> ${imie} <br>
+        <b> Nazwisko: </b> ${nazwisko} <br>
+        <b> Wiek: </b> ${wiek} <br>
+        <b> Email: </b> ${email} <br>
+        <b> Płeć: </b> ${plec} <br>
+        <b> Najczęściej używana technologia: </b> ${technologia} <br>
+        <b> Języki programowania, które umiesz w stopniu conajmniej podstawowym: </b> ${jezyki} <br>
+        <b> Ocena: </b> ${ocena}/100 <br>
+        <b> Opinia: </b> ${opinia} <br>
+        <b> Data wypełnienia formularza: </b> ${data}`;
+        
     }
-
-    let ocena = document.querySelector("#ocena").value;
-    let plik = document.querySelector("#plik")[0].value;
-    let opinia = document.querySelector("#tekst").value;
-    let data = document.querySelector("#data").value;
-
-
-    document.querySelector("#dane").innerHTML = `Imię: ${imie} <br>
-    Nazwisko: ${nazwisko} <br>
-    Wiek: ${wiek} <br>
-    Email: ${email} <br>
-    Płeć: ${plec} <br>
-    Najczęściej używana technologia: ${technologia} <br>
-    Języki programowania, które umiesz w stopniu conajmniej podstawowym: ${jezyki} <br>
-    Ocena: ${ocena} <br>
-    Plik: ${plik} <br>
-    Opinia: ${opinia} <br>
-    Data: ${data}`;
+    else
+    {
+        alert("Wysłanie formularza jest niemożliwe bez zgody na przetwarzanie danych osobowych");
+    }
 }
-
 
 
 
